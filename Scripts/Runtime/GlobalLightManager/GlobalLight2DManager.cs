e5b06561a4f912252cdef65b09229d5470da275a
shikeik <30600841+shikeik@users.noreply.github.com>
2024-03-20 14:19:09 +0800

    V0.2.0

--- Scripts/Runtime/GlobalLightManager/GlobalLight2DManager.cs
+++ Scripts/Runtime/GlobalLightManager/GlobalLight2DManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -10,7 +12,7 @@ namespace GoldSprite.UnityPlugins.DayNightCycleSystem {
     /// </summary>
     public class GlobalLight2DManager : MonoBehaviour {
         [Header("引用")]
-        public Light2D light2D;
+        public Light2D globalLight;
 
         [Header("配置")]
         [Tooltip("gameMinPerDay表示每现实时间{0}分钟为游戏内24h.")]
@@ -20,8 +22,10 @@ namespace GoldSprite.UnityPlugins.DayNightCycleSystem {
         [Range(0, 1)]
         public float lightingRange_max = 1;
         public bool manualMode;
-        [Range(0, 1)]
-        public float manualLighting;
+        [Range(0, 24)]
+        public float manualGameTimeHours;
+        public Color dayColor = Color.blue;
+        public Color nightColor = Color.red;
 
         [Header("实时")]
         public double SystemTimeSeconds;
@@ -30,45 +34,70 @@ namespace GoldSprite.UnityPlugins.DayNightCycleSystem {
         public double currentSystemMins;
         [Header("游戏归一化当前时间")]
         public double gameTimeNormalized;
-        public float GameTimeSeconds;
+        public float gameTimeHours;
         [Header("游戏时间")]
-        public string GameTime;
+        public string gameTime;
         [Range(0, 1)]
         [Header("当前归一化日照强度")]
         public float currentLightingNormalized;
         [Range(0, 1)]
         [Header("当前日照强度")]
         public float currentLighting;
-        public float CurrentLighting => manualMode ? manualLighting : currentLighting;
-
+        [Header("当前光照颜色偏移")]
+        public Color clightColor;
+        public Color cDayColor;
+        public Color cNightColor;
+        public List<Light2D> environmentLights => FindObjectsOfType<Light2D>().Where(p=>p!= globalLight).ToList();
 
         void Start()
         {
-            light2D = GetComponent<Light2D>();
+            globalLight = GetComponent<Light2D>();
         }
 
         void Update()
         {
+            //计算当前系统分钟数
             SystemTimeSeconds = DateTime.Now.TimeOfDay.TotalSeconds;
             SystemTime = new DateTime((long)(SystemTimeSeconds * TimeSpan.TicksPerSecond)).ToString("HH:mm:ss");
             currentSystemMins = SystemTimeSeconds / 60f;
 
+            //计算当前游戏时
             gameTimeNormalized = currentSystemMins % gameMinPerDay / gameMinPerDay;
-            GameTimeSeconds = (float)(gameTimeNormalized * 60 * 60 * 24);
-            GameTime = new DateTime((long)(GameTimeSeconds * TimeSpan.TicksPerSecond)).ToString("HH:mm:ss");
+            gameTimeHours = manualMode ? manualGameTimeHours : (float)(gameTimeNormalized * 24);
+            gameTime = new DateTime((long)(gameTimeHours * TimeSpan.TicksPerHour)).ToString("HH:mm:ss");
 
-            currentLightingNormalized = GetDayTimeLighting(gameTimeNormalized);
+            //计算光照/红蓝光强度
+            currentLightingNormalized = GetDayTimeLighting(gameTimeHours);
             currentLighting = LimitLightingRange(currentLightingNormalized);
-            light2D.intensity = currentLighting;
+            clightColor = GetLightingColor(currentLightingNormalized);
 
-            if (manualMode)
-                light2D.intensity = manualLighting;
+
+            globalLight.intensity = currentLighting;
+            globalLight.color = clightColor;
+
+            //其他光源
+            foreach(var light in environmentLights) {
+                light.intensity = 1 - currentLighting;
+            }
         }
 
-
-        public float GetDayTimeLighting(double pams)
+        //0/24点红光弱蓝光强, 12点蓝光弱红光强
+        private Color GetLightingColor(float lighting)
         {
-            var rad = pams * Math.PI * 2;
+            var sunIntensity = lighting;
+            var moonIntensity = 1 - lighting;
+
+            cDayColor = dayColor * sunIntensity; cDayColor.a = 1;
+            cNightColor = nightColor * moonIntensity; cNightColor.a = 1;
+
+            clightColor = Color.Lerp(cNightColor, cDayColor, lighting);
+            return clightColor;
+        }
+
+        public float GetDayTimeLighting(float hours)
+        {
+            var normalizeHours = hours / 24f;
+            var rad = normalizeHours * Math.PI * 2;
             var cos = Math.Cos(rad);
             var result = (float)(-cos / 2f + 0.5f);
             return result;
@@ -80,4 +109,6 @@ namespace GoldSprite.UnityPlugins.DayNightCycleSystem {
             return result;
         }
     }
+
+
 }
